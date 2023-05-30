f6cbaaa4afc3fdf5773198f53f7f273599c2bb67
Fernando <fer3443@gmail.com>
2023-05-30 15:03:30 -0300

    componente y css creados

---
+++ obsidian-tech/src/components/grillaDeProductos/ProductGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../grillaDeProductos/ProductGrid.css'
+
+export const ProductGrid = () => {
+  return (
+    <div className='gridContainer'>
+			<h1>Grilla de Productos</h1>
+		</div>
+  )
+}
--- obsidian-tech/src/page/InicioPage.jsx
+++ obsidian-tech/src/page/InicioPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { ProductGrid } from '../components/grillaDeProductos/ProductGrid'
 
 const InicioPage = () => {
   return (
-    <div>InicioPage</div>
+    <ProductGrid/>
   )
 }
 
