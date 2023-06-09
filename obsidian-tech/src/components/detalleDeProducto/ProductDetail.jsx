be2c240feca2cdeef3efc8a55d5a7b8bd1e91cc2
Fernando <fer3443@gmail.com>
2023-06-09 14:33:57 -0300

    conexion con endPoint exitosa y cambios de estilos

--- obsidian-tech/src/components/detalleDeProducto/ProductDetail.jsx
+++ obsidian-tech/src/components/detalleDeProducto/ProductDetail.jsx
@@ -5,35 +5,49 @@ import { useParams, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faHeart } from '@fortawesome/free-solid-svg-icons';
 
+import { getProductByIdFromDb } from '../../services/api';
 import '../detalleDeProducto/ProductDetail.css';
-import { getProductFromDb } from '../../services/api';
+
 
 
 const ProductDetail = () => {
 
 	const {id} = useParams();
-	// const [data, setData] = useState([]);
-	// console.log(data)
-	// useEffect(() => {
-	// 	getProductFromDb(id)
-	// 	.then((res => setData(res)))
-	// }, [])
+	
+	const [data, setData] = useState([]);
+	const [num, setNum] = useState(1)
+
+	function sumar(){
+		setNum( num + 1)
+	}
+	function restar(){
+		if(num == 1){
+			setNum(1)
+		}else{
+			setNum(num - 1)
+		}
+	}
+	useEffect(() => {
+		getProductByIdFromDb(id)
+		.then((res => setData(res)))
+		.catch(error => console.log(error))
+	}, [])
 	
   return (
     <div className='containerProductDetail'>
 			<div className='containerDetail'>
 				<Link to={'/'} className='linkBack'><FontAwesomeIcon icon={faArrowLeft}/>Inicio</Link>
 				<div className='boxCateogy'>
-					<div className='category'>categoria</div>
+					<div className='category'>{data.categoria}</div>
 					<button><FontAwesomeIcon icon={faHeart} /></button>
 					<p>Agregar a lista de deseos.</p>
 				</div>
-				<h2>Nombre del Producto.</h2>
+				<h2>{data.nombre}</h2>
 				<div  className='boxDetail'>
-					<p>TYPE: categoria</p>
-					<p>ID: 1234567</p>
+					<p>TYPE: {data.categoria}</p>
+					<p>Stock: {data.stock}</p>
 				</div>
-				<h6>Descripcion del producto: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ipsa earum a, odit quidem repellat. Harum, ea. Excepturi, quos iure!</h6>
+				<h6>Descripcion del producto: {data.descripcion}</h6>
 				<div className='boxInputSelect'>
 					<label htmlFor="color">Color:</label>
 					<select name="" id="color">
@@ -42,13 +56,13 @@ const ProductDetail = () => {
 					</select>
 				</div>
 				<div className='boxPrice1'>
-					<span className='precio'>$ 10000</span>
+					<span className='precio'>$ {data.precio}</span>
 					<div className='boxQuantity'>
 						<p>Cantidad:</p>
 						<div className='lbl'>
-							<button>-</button>
-							<span className='num'>1</span>
-							<button>+</button>
+							<button onClick={restar}>-</button>
+							<span className='num'>{num}</span>
+							<button onClick={sumar}>+</button>
 						</div>
 					</div>
 				</div>
@@ -60,7 +74,7 @@ const ProductDetail = () => {
 			<div className='containerPictures'>
 				<div className='borderColorBox'>
 					<div className='containerImg'>
-						<img src="https://zonagamer.co/wp-content/uploads/2021/06/J-10L1-.png" alt="" />
+						<img src={data.urlImg} alt="" />
 					</div>
 				</div>
 			</div>
--- obsidian-tech/src/services/api.js
+++ obsidian-tech/src/services/api.js
@@ -3,7 +3,7 @@ export async function getAllProductsFromDB() {
     return await resultado.json()
 }
 
-export async function getProductFromDb() {
+export async function getProductByIdFromDb(id) {
     const resultado = await fetch(`http://localhost:1000/api/accesorio/${id}`)
     return await resultado.json();
 }
