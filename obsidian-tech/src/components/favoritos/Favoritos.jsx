ee2ecb3d7e84e8acd20fdaa7e802316705870df6
Fernando <fer3443@gmail.com>
2023-07-11 22:19:59 -0300

    cambios menores

--- obsidian-tech/src/components/favoritos/Favoritos.jsx
+++ obsidian-tech/src/components/favoritos/Favoritos.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../favoritos/Favoritos.css';
+import { Link } from 'react-router-dom';
 
 const Favoritos = () => {
   return (
-    <div>Favoritos</div>
+    <div className='containerGral'>
+        <div className='ventanaFav'>
+            <div className='boxTitleFav'>
+							<Link to="/">Inicio</Link>
+							<h3>Lista de Favoritos</h3>
+							<p>Aquí podras ver todos los productos que hayas seleccionado, así como también quitarlos si encuentras uno mejor!.</p>
+						</div>
+						<div className='boxFav'>
+							<h3>Favoritos (1)</h3>
+						</div>
+        </div>
+    </div>
   )
 }
 
