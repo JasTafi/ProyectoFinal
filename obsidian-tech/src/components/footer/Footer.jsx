80e75264f9d9a29311d5104bd292032b86bbf6a3
Fernando <fer3443@gmail.com>
2023-07-14 14:55:09 -0300

    estilos agregados

--- obsidian-tech/src/components/footer/Footer.jsx
+++ obsidian-tech/src/components/footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import '../footer/Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='containerGralFooter'>
+        <div className='circulo izq'></div>
+        <div className='circulo der'></div>
+        <div className='containerFooter'>
+            <div className='containerLogFooter'>
+                <h2>Obsidian-Tech</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nesciunt perspiciatis quos temporibus fuga sed aspernatur qui fugit labore in!</p>
+            </div>
+            <div className='containerLinksFooter'>
+                <h4>NAVEGACIÓN:</h4>
+                <ul>
+                    <Link className='linksFoot' to={"/"}>Home</Link>
+                    <Link className='linksFoot' to={"recContraseña"}>Recuperar contraseña </Link>
+                    <Link className='linksFoot' to={"nosotros"}>Nosotros </Link>
+                    <Link className='linksFoot' to={"favoritos"}>Favoritos </Link>
+                </ul>
+            </div>
+            <div className='containerSocialFooter'>
+                <h4>SEGUINOS:</h4>
+                <ul>
+                    <Link className='linksFoot' to={"https://www.youtube.com/"}>youtube</Link>
+										<Link className='linksFoot' to={"https://www.instagram.com/"}>instagram</Link>
+										<Link className='linksFoot' to={"https://www.facebook.com/"}>facebook</Link>
+										<Link className='linksFoot' to={"https://twitter.com/"}>twitter</Link>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
