503d5cb07cfa71727cd155227fc780d9f9350e2c
Fernando <fer3443@gmail.com>
2023-07-25 01:03:35 -0300

    mergeo con la Dev

--- obsidian-tech/src/services/user_service.jsx
+++ obsidian-tech/src/services/user_service.jsx
@@ -61,22 +61,6 @@ async function GetFavoriteProduct({ id, token}) {
   return results
   
 }
-async function DeleteFavoriteById({ id, productId, token }) {
-  const body = JSON.stringify({
-    productId,
-  });
-  const response = await fetch(`${Puerto.URL_LOCAL}/user/favorites/${id}`, {
-    method: 'PUT',
-    headers: {
-      'content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: body,
-  });
-  return await response.json();
-}
-
-<<<<<<< HEAD
 // Eliminar un producto de la lista de favoritos
 async function DeleteFavoriteById({ id, productId, token }) {
   const body = JSON.stringify({
@@ -93,6 +77,4 @@ async function DeleteFavoriteById({ id, productId, token }) {
   return await response.json();
 }
 
-=======
->>>>>>> origin/footer
 export { CreateUser, Login, AddFavoriteProduct, GetFavoriteProduct, DeleteFavoriteById };
