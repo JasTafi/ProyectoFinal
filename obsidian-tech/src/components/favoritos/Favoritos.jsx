901fbca10199c334e18ad8cce1e92be7c15697b2
Fernando <fer3443@gmail.com>
2023-08-06 12:24:22 -0300

    xx

--- obsidian-tech/src/components/favoritos/Favoritos.jsx
+++ obsidian-tech/src/components/favoritos/Favoritos.jsx
@@ -43,7 +43,7 @@ const Favoritos = () => {
 								<h3>Favoritos {fav.length}</h3>
 								<div className='containerCardFav'>
 								{
-								(fav.length == 0 ? <h2 className='favEmpty'>No tienes productos agregados a favoritos!</h2> : <CardFavorites fav={fav} />)
+								(fav.length == 0 ? <div className='favEmpty'><h2>No tienes productos agregados a favoritos!</h2></div> : <CardFavorites fav={fav} />)
 								}
 								</div>
 								{
