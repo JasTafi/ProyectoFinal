80c3bdcc4cca9837a66f7e71d3b0a863c9bc7333
Gaston Monteiro Eichele <monteirogaston@gmail.com>
2023-08-30 23:16:06 -0300

    Se agrego logica a lista de productos en administracion

--- obsidian-tech/src/components/toDoListAdministracion/toDoListAdministration.jsx
+++ obsidian-tech/src/components/toDoListAdministracion/toDoListAdministration.jsx
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
-
-import { useParams, Link } from 'react-router-dom';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faHeart } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react'
 
 import { getAllProductsFromDB } from '../../services/api'
-import '../toDoListAdministracion/toDoListAdministration.css';
 
-const DataList = () => {
-  const [dataList, setDataList] = useState([]);
+import '../toDoListAdministracion/toDoListAdministration.css'
+
+const dataList = () => {
+
+  const [dataApi, setDataApi] = useState([])
 
   useEffect(() => {
     getAllProductsFromDB()
@@ -18,40 +15,29 @@ const DataList = () => {
     })
     .catch(error => console.log(error))
   }, [])
-
-  const fetchData = () => {
-    fetch('/api/get-data')
-      .then((response) => response.json())
-      .then((data) => {
-        setDataList(data);
-      })
-      .catch((error) => {
-        console.error('Error al obtener los datos:', error);
-      });
-  };
+  
 
   return (
     <div className='divPadreDataList'>
       <h3>LISTA DE PRODUCTOS</h3>
-      <hr></hr>
-      {dataList.length === 0 ? (
-        <p>No hay datos disponibles.</p>
-      ) : (
-        <ul>
-          {dataList.map((data, index) => (
-            <li key={index}>
-              <p>Nombre: {data.nombre}</p>
-              <p>Categoría: {data.categoria}</p>
-              <p>Precio: {data.precio}</p>
-              <p>Stock: {data.stock}</p>
-              <p>Descripción: {data.descripcion}</p>
-              <img src={data.imagen} alt="Imagen" />
-            </li>
-          ))}
-        </ul>
-      )}
+      <hr />
+      <ul>
+        {dataApi.map((item) => (
+          <li key={item.id}>
+            <p>Nombre: {item.nombre}</p>
+            <p>Categoría: {item.categoria}</p>
+            <p>Precio: {item.precio}</p>
+            <p>Stock: {item.stock}</p>
+            <p>Descripción: {item.descripcion}</p>
+            <div className='botonImagen'>
+            <p>Imagen:</p>
+            <a href={item.urlImg} target="_blank" rel="noopener noreferrer" className="image-link-button">Ver imagen</a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default DataList;
+export default dataList;
