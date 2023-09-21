1710292f38a522b6ba41e6b69e33928cdba81bf9
Gaston Monteiro Eichele <monteirogaston@gmail.com>
2023-09-21 20:36:39 -0300

    Se modifico el formao y se agrego boton de editar

--- obsidian-tech/src/components/toDoListAdministracion/toDoListAdministration.jsx
+++ obsidian-tech/src/components/toDoListAdministracion/toDoListAdministration.jsx
@@ -1,120 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import Accordion from 'react-bootstrap/Accordion';
+import React, { useEffect, useState } from 'react';
 
-import { getAllProductsFromDB } from '../../services/api'
+import { getAllProductsFromDB } from '../../services/api';
 
-import '../toDoListAdministracion/toDoListAdministration.css'
+import '../toDoListAdministracion/toDoListAdministration.css';
+
+import CategoryAccordion from '../categoryAccordion/CategoryAccordion';
 
 const dataList = () => {
-
-  const [dataApi, setDataApi] = useState([])
+  const [dataApi, setDataApi] = useState([]);
 
   useEffect(() => {
     getAllProductsFromDB()
-    .then(({data}) => {
-      setDataApi(data)
-    })
-    .catch(error => console.log(error))
-  }, [])
+      .then(({ data }) => {
+        setDataApi(data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
+  // Obtener la lista de categorías únicas
+  const uniqueCategories = [...new Set(dataApi.map(item => item.categoria))];
 
   return (
-    <div className='divPadreDataList'>
+    <div className="divPadreDataList">
       <h3>LISTA DE PRODUCTOS</h3>
-  
-      <Accordion defaultActiveKey="0">
-<Accordion.Item eventKey="1">
-<Accordion.Header>Teclados</Accordion.Header>
-<Accordion.Body>
-
-        
-          
-        
-        <ul>
-        {dataApi.map((item) => (
-
-            <Accordion className='accordion'  defaultActiveKey="0">
-              <Accordion.Item eventKey="1">
-                <Accordion.Header className='accordion'>{item.nombre}</Accordion.Header>
-                <Accordion.Body className='accordion'>
-                  <li key={item.id}>
-                    <div className='divProducto'>
-                      <h4>Nombre:</h4>
-                      <p>{item.nombre}</p>
-                    </div>
-                    <div className='divProducto'>
-                      <h4>Categoría:</h4>
-                      <p>{item.categoria}</p>
-                    </div>
-                    <div className='divProducto'>
-                      <h4>Precio:</h4>
-                      <p>${item.precio}</p>
-                    </div>
-                    <div className='divProducto'>
-                    <h4>Stock:</h4>
-                    <p>{item.stock} u.</p>
-                  </div>
-                    <div className='divProducto'>
-                      <h4>Descripcion:</h4>
-                    <p>{item.Descripcion}</p>
-                  </div>
-                    <div className='botonImagen'>
-                      <h4>Imagen:</h4>
-                      <a href={item.urlImg} target="_blank" rel="noopener noreferrer" className="image-link-button">Ver imagen</a>
-                    </div>
-                  </li>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-                    ))}
-        </ul>
-        
-</Accordion.Body>
-</Accordion.Item>
-</Accordion>
       <hr />
-      <ul>
-        {dataApi.map((item) => (
+      {uniqueCategories.map(category => (
+        <CategoryAccordion key={category} category={category} products={dataApi} />
+      ))}
+    </div>
+  );
+};
 
-<hr />
-    // <Accordion className='accordion'  defaultActiveKey="0">
-    //   <Accordion.Item eventKey="1">
-    //     <Accordion.Header className='accordion'>{item.nombre}</Accordion.Header>
-    //     <Accordion.Body className='accordion'>
-    //       <li key={item.id}>
-    //         <div className='divProducto'>
-    //           <h4>Nombre:</h4>
-    //           <p>{item.nombre}</p>
-    //         </div>
-    //         <div className='divProducto'>
-    //           <h4>Categoría:</h4>
-    //           <p>{item.categoria}</p>
-    //         </div>
-    //         <div className='divProducto'>
-    //           <h4>Precio:</h4>
-    //           <p>${item.precio}</p>
-    //         </div>
-    //         <div className='divProducto'>
-    //           <h4>Stock:</h4>
-    //           <p>{item.stock} u.</p>
-    //         </div>
-    //         <div className='divProducto'>
-    //           <h4>Descripcion:</h4>
-    //           <p>{item.Descripcion}</p>
-    //         </div>
-    //         <div className='botonImagen'>
-    //           <h4>Imagen:</h4>
-    //           <a href={item.urlImg} target="_blank" rel="noopener noreferrer" className="image-link-button">Ver imagen</a>
-    //         </div>
-    //       </li>
-    //     </Accordion.Body>
-    //   </Accordion.Item>
-    // </Accordion>
-            ))}
-            </ul>
-            
-          </div>
-  )
-}
 
 export default dataList;
