06f17a9bebe03704575fef4a9d480abf809ad1d7
Fernando <fer3443@gmail.com>
2023-11-15 23:01:18 -0300

    cambios varios en estilos

--- obsidian-tech/src/components/filtroNavegacion/FilterComponent.jsx
+++ obsidian-tech/src/components/filtroNavegacion/FilterComponent.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
