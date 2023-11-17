8fe26ce3f314647797dc8ef5806653e9d8903fc2
Fernando <fer3443@gmail.com>
2023-11-17 17:19:50 -0300

    componente About, estilos y responsive agregado

---
+++ obsidian-tech/src/components/about/About.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import '../about/About.css'
+const About = () => {
+  return (
+    <>
+      <section className="section">
+        <div className="about-container grid">
+          <div className="about-content">
+            <h2 className="about-title">Quienes somos!</h2>
+            <h4 className="about-subtitle">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+              laudantium excepturi molestiae nemo repellat id magni, eaque
+              corporis neque quo distinctio autem nostrum tenetur? Nam sit
+              tenetur possimus dolorum vitae.
+            </h4>
+            <div className="about-card-container">
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+              <div className="about-card">
+                <img src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg" alt="" />
+                <h5>Nombre de Ejemplo</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore debitis molestiae mollitia labore nostrum?</p>
+                <span>Full-Stack Developer</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
--- obsidian-tech/src/page/NosotrosPage.jsx
+++ obsidian-tech/src/page/NosotrosPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import About from '../components/about/About'
 
 const NosotrosPage = () => {
   return (
-   <div>Nosotros</div>
+    <About/>
   )
 }
 
