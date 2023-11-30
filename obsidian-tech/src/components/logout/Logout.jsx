8d5c28138328c70f836552cf5c5ef086c1e2a5fc
rodrigo <rodrigoaragon1977@gmail.com>
2023-11-30 11:24:46 -0300

    Cierre de sesión

--- obsidian-tech/src/components/logout/Logout.jsx
+++ obsidian-tech/src/components/logout/Logout.jsx
@@ -1,20 +1,37 @@
 import React, { useContext } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import { DataProvider } from "../../context/DataContext";
+import { Notification } from "../../services/tostifyNot";
 
 import "../logout/Logout.css";
 
 export const Logout = () => {
-  const { userInfo } = useContext(DataProvider)
+
+  const { userInfo, setUserInfo, setShowModal } = useContext(DataProvider)
+
+  const handleLogout = () => {
+    setUserInfo({
+      islogged: false,
+      user: {},
+    });
+
+    //Borro la informacion en localStorage
+    localStorage.removeItem("user");
+
+    //Cierro el modal login
+    setShowModal(false);
+
+    //Notifico al usuario que cerro sesión con exito
+    Notification({ message: "Cierre de sesión exitoso", type: "success" });
+  }
   return (
     <section className="logout-section">
       <div className="logout-container">
         <h5>Sesion iniciada</h5>
         <span>{userInfo.user.email}</span>
-        <button className="btn-logout">cerrar sesion</button>
+        <button className="btn-logout" onClick={handleLogout}>cerrar sesion</button>
       </div>
     </section>
   );
