62d38cf3ac2bd8b8ad93eed844ba6803d6cb27c3
JasTafi <jose.sv@gmail.com>
2023-12-05 23:54:58 -0300

    Mergeo Footer - About

--- obsidian-tech/src/components/about/About.jsx
+++ obsidian-tech/src/components/about/About.jsx
@@ -17,21 +17,71 @@ const About = () => {
             <div className="about-content">
               <h2 className="about-title">Quienes somos!</h2>
               <h4 className="about-subtitle">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-                laudantium excepturi molestiae nemo repellat id magni, eaque
-                corporis neque quo distinctio autem nostrum tenetur? Nam sit
-                tenetur possimus dolorum vitae.
+                Obsidian-Tech, se destaca por ofrecer servicios de comercialización de tecnología con un enfoque en la conveniencia para sus clientes. Su objetivo es proporcionar los últimos productos del mercado a precios competitivos y con opciones de financiación. Además, cuentan con un eficiente servicio de envíos puerta a puerta, asegurando que los productos lleguen puntualmente a los clientes. 
               </h4>
+              <h2 className="about-title">Nuestros valores:</h2> 
+              <h4 className="about-subtitle">
+                Transparencia por defecto, Mejorar Constantemente, Ser un hacedor sin ego, 
+                Mostrar gratitud, Elegir el optimismo. 
+              </h4>
+                <h2 className="about-title">Conoce a los integrantes de nuestro equipo:</h2>
+                
+                           
               <div className="about-card-container">
                 <div className="about-card">
                   <img
-                    src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
+                    src="/src/assets/Fer-Arroyo.jpeg"
                     alt=""
                   />
-                  <h5>Nombre de Ejemplo</h5>
+                  <h5>Fernando Arroyo</h5>
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis inventore debitis molestiae mollitia labore nostrum?
+                    Founder CEO -  (San Miguel de Tucumán - Tucumán)
+
+                  </p>
+                  <span>Full-Stack Developer</span>
+                </div>
+                <div className="about-card">
+                  <img
+                    src="/src/assets/Rodrigo Aragon.jpg"
+                    alt=""
+                  />
+                  <h5>Rodrigo Aragón</h5>
+                  <p>
+                  Founder CEO -  (San Miguel de Tucumán - Tucumán)
+                  </p>
+                  <span>Full-Stack Developer</span>
+                </div>
+                <div className="about-card">
+                  <img
+                    src="/src/assets/Gaston Monteiro.jpg"
+                    alt=""
+                  />
+                  <h5>Gastón Monteiro</h5>
+                  <p>
+                  Director of Engineering: Platform & Technology (Tucumán)
+                  </p>
+                  <span>Full-Stack Developer</span>
+                </div>
+                <div className="about-card">
+                  <img
+                    src="/src/assets/Jose Sanchez.jpg"
+                    alt=""
+                  />
+                  <h5>José Sánchez</h5>
+                  <p>
+                    Senior Engineer (Tafi Viejo - Tucumán)
+                  </p>
+                  <span>Full-Stack Developer</span>
+                </div>
+                <div className="about-card">
+                  <img
+                    src="/src/assets/Gonzalo_Velardez.jpg"
+                    alt=""
+                  />
+                  <h5>Gonzalo Velardez</h5>
+                  <p>
+                    Technical Customer Advocate.
+                    San Miguel de Tucuman
                   </p>
                   <span>Full-Stack Developer</span>
                 </div>
@@ -40,59 +90,23 @@ const About = () => {
                     src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
                     alt=""
                   />
-                  <h5>Nombre de Ejemplo</h5>
+                  <h5>Blas Stanciuc</h5>
                   <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Quis inventore debitis molestiae mollitia labore nostrum?
                   </p>
                   <span>Full-Stack Developer</span>
                 </div>
+
                 <div className="about-card">
                   <img
                     src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
                     alt=""
                   />
-                  <h5>Nombre de Ejemplo</h5>
+                  <h5>Blas Stanciuc</h5>
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis inventore debitis molestiae mollitia labore nostrum?
-                  </p>
-                  <span>Full-Stack Developer</span>
-                </div>
-                <div className="about-card">
-                  <img
-                    src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
-                    alt=""
-                  />
-                  <h5>Nombre de Ejemplo</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis inventore debitis molestiae mollitia labore nostrum?
-                  </p>
-                  <span>Full-Stack Developer</span>
-                </div>
-                <div className="about-card">
-                  <img
-                    src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
-                    alt=""
-                  />
-                  <h5>Nombre de Ejemplo</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis inventore debitis molestiae mollitia labore nostrum?
-                  </p>
-                  <span>Full-Stack Developer</span>
-                </div>
-                <div className="about-card">
-                  <img
-                    src="https://objetivoligar.com/wp-content/uploads/2017/03/blank-profile-picture-973460_1280.jpg"
-                    alt=""
-                  />
-                  <h5>Nombre de Ejemplo</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quis inventore debitis molestiae mollitia labore nostrum?
-                  </p>
+                    Staff Engineer.(Mar del Plata - Buenos Aires)
+                   </p>
                   <span>Full-Stack Developer</span>
                 </div>
               </div>
--- obsidian-tech/src/components/footer/Footer.jsx
+++ obsidian-tech/src/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import '../footer/Footer.css';
 
+
 const Footer = () => {
   return (
     <div className='containerGralFooter'>
@@ -12,10 +13,25 @@ const Footer = () => {
         <div className='containerFooter'>
             <div className='containerLogFooter'>
                 <h2>Obsidian-Tech</h2>
-                <p>Tenemos 2 años de experiencia en el rubro de Ventas de Tecnología. Nuestro objetivo es dar lo mejor de nosotros para tus proyectos. Buscamos  fugit labore in!</p>
-								<h6>Hecho por alumnos de la comision 28i RollingCode</h6>
+                <p>Somos expertos en ventas de tecnología, nos dedicamos a proporcionar lo mejor para los proyectos de nuestros clientes. Activamente buscamos emprendedores interesados en unirse a nuestro proyecto. Contáctanos para más información.</p>
+								<h6>Desarrollado por alumnos de la comisión 28i de:</h6>
+                
+                <div className="logoRolling">
+                  <span>{'<'}{'>'}</span>
+                </div>
+
+                <a className='linkRolling' href="https://web.rollingcodeschool.com" target="_blank" rel="noopener noreferrer">
+                  RollingCode SCHOOL
+                </a>
+                
+                {/* <div className="logoRolling">
+                  <span>{'<'}{'>'}</span>
+                </div> */}
+               
+                  {/* <img src="https://web.rollingcodeschool.com/wp-content/uploads/2023/10/RollingCode-Logo-3.svg" target="_blank" rel="noopener noreferrer" /> */}
+                                             
             </div>
-            <div className='containerLinksFooter'>
+            {/* <div className='containerLinksFooter'>
                 <h4>NAVEGACIÓN:</h4>
                 <ul>
                     <Link className='linksFoot' to={"/"}>Home</Link>
@@ -23,16 +39,27 @@ const Footer = () => {
                     <Link className='linksFoot' to={"nosotros"}>Nosotros </Link>
                     <Link className='linksFoot' to={"favoritos"}>Favoritos </Link>
                 </ul>
+            </div> */}
+            <div className='containerContactos'>
+                <h4>CONTACTO:</h4>
+                <ul>
+                     <li class="fas fa-home me-3"></li>General Paz 576, Piso 8, oficina 2
+                     <li class="fas fa-home me-3"></li>San Miguel de Tucumán, Argentina
+                     <li class="fas fa-envelope me-3"></li>info@Obsidian-Tech.com.ar 
+                     <li class="fas fa-phone me-3"></li> +54 381 606-4103 
+                </ul>
+                <p>Copyright &copy; 2023 Obsidian-Tech. Todos los derechos reservados</p>
             </div>
+
             <div className='containerSocialFooter'>
                 <h4>SEGUINOS:</h4>
                 <ul>
-                    <Link className='linksFoot' to={"https://www.youtube.com/"}>youtube</Link>
-										<Link className='linksFoot' to={"https://www.instagram.com/"}>instagram</Link>
-										<Link className='linksFoot' to={"https://www.facebook.com/"}>facebook</Link>
-										<Link className='linksFoot' to={"error404"}>twitter</Link>
+                    <Link className='linksFoot' to={"https://www.youtube.com/"}>Youtube</Link>
+										<Link className='linksFoot' to={"https://www.instagram.com/"}>Instagram</Link>
+										<Link className='linksFoot' to={"https://www.facebook.com/"}>Facebook</Link>
+										<Link className='linksFoot' to={"error404"}>Twitter</Link>
                 </ul>
-            </div>
+           </div>
         </div>
     </div>
   )
